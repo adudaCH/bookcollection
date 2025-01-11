39d58ac3fc8d28c7b894978e1ffc302bbe9e71ee
adudaCH <johndoe@example.com>
2025-01-11 20:08:58 +0200

    toast

--- src/sevices/toastify.ts
+++ src/sevices/toastify.ts
@@ -1,28 +1,23 @@
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-
-
-
 export function successMsg(msg: string) {
-	toast.success(msg, {
-position: "top-center",
-autoClose: 3000,
-hideProgressBar: false,
-closeOnClick: true,
-pauseOnHover: true,
-draggable: true,
-progress: undefined,
-theme: "colored",
-});
+    toast.success(msg, {
+        position: "top-center",
+        autoClose: 4000,
+        hideProgressBar: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
 }
 
-
 export function errorMsg(msg: string) {
-	toast.error(msg, {
-		position: "top-center",
-		autoClose: 4000,
-		theme: "colored",
-	});
+    toast.error(msg, {
+        position: "top-center",
+        autoClose: 4000,
+        theme: "colored",
+        hideProgressBar: true,
+    });
 }
